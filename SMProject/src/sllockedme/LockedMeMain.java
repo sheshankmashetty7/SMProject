20027d8c216ae8cbccb36f3856216fb7c41d43fc
sheshankmashetty7 <110464190+sheshankmashetty7@users.noreply.github.com>
2022-08-04 21:29:00 +0530

    First Commit

---
+++ SMProject/src/sllockedme/LockedMeMain.java
@@ -0,0 +1,16 @@
+package sllockedme;
+
+public class LockedMeMain {
+
+    public static void main(String[] args) {
+
+        // Create "main" folder if not present in current folder structure
+        FileOperations.createMainFolderIfNotPresent("main");
+
+        MenuOptions.printWelcomeScreen("LockedMe", "Sheshank");
+
+        HandleOptions.handleWelcomeScreenInput();
+    }
+
+
+}
